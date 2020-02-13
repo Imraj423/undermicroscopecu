7ece2a71a6610eac08e84047a056bc209f0bdc00
Imraj423 <dahqniss@gmail.com>
2020-02-13 11:53:28 -0500

    Initial Commit

---
+++ cUser/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from custom_user.models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'display_name',
+            'first_name',
+            'last_name',
+            'username',
+            'age'
+        ]
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput)
